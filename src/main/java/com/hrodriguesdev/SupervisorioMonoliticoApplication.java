98f5b5ee2e820f823021912904fa4457f557f7e0
Henrique Rodrigues <hyck.rocha@gmail.com>
2022-08-01 18:04:08 -0300

    Created project

---
+++ src/main/java/com/hrodriguesdev/SupervisorioMonoliticoApplication.java
@@ -0,0 +1,13 @@
+package com.hrodriguesdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupervisorioMonoliticoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupervisorioMonoliticoApplication.class, args);
+	}
+
+}
